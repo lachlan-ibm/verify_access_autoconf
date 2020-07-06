a8b4685da4b42dfe5b9fb3180294cacfa0927bda
Lachlan Gleeson <lgleeson@au1.ibm.com>
2020-07-07 08:44:53 +1000

    Add extra pip index for testing

--- .tests/import_test.sh
+++ .tests/import_test.sh
@@ -7,6 +7,6 @@ python setup.py sdist bdist_wheel
 
 pip install `ls $HOME/dist/*.whl` --extra-index https://${ART_API_USER}:${ART_API_KEY}@eu.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-components-pypi-local/simple
 
-cd "`basename $0`"
+cd "`dirname $0`"
 
 python import_test.py
