6749a0a55b0fecd40c2e9cd71206097a65229eb0
Lachlan Gleeson <lgleeson@au1.ibm.com>
2020-07-07 08:47:52 +1000

    Commit to components artifactory for now

--- .deploy.sh
+++ .deploy.sh
@@ -2,4 +2,4 @@
 
 export HOME=$( realpath . )
 python setup.py sdist bdist_wheel
-twine upload --verbose --repository-url https://eu.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-isam-devops-team-pypi-local/ -u $ART_API_USER -p $ART_API_KEY dist/*
+twine upload --verbose --repository-url https://eu.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-components-pypi-local/ -u $ART_API_USER -p $ART_API_KEY dist/*
