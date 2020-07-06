d60f48234181d302af1d48ad754da7b7c509d756
Lachlan Gleeson <lgleeson@au1.ibm.com>
2020-07-07 09:14:15 +1000

    Move to devops artifactory

--- .tests/import_test.sh
+++ .tests/import_test.sh
@@ -5,7 +5,7 @@
 export HOME=$( realpath . )
 python setup.py sdist bdist_wheel
 
-pip install `ls $HOME/dist/*.whl` --extra-index https://${ART_API_USER}:${ART_API_KEY}@na.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-isam-devops-team-pypi-local/
+pip install `ls $HOME/dist/*.whl` --extra-index https://${ART_API_USER}:${ART_API_KEY}@na.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-isam-devops-team-pypi-local/simple
 cd "`dirname $0`"
 
 python import_test.py
