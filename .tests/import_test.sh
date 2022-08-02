e6687129d6ea098cd76136a24665e862363b0f68
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-02 14:28:37 +1000

    Refactoring

--- .tests/import_test.sh
+++ .tests/import_test.sh
@@ -1,13 +1,7 @@
-#!/bin/sh
-
 #!/bin/bash
 export PIP_EXTRA_INDEX_URL="https://${ART_API_USER}:${ART_API_KEY}@na.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-isam-devops-team-pypi-local/simple"
-
-export HOME=$( realpath . )
 python setup.py sdist bdist_wheel
-
-pip install `ls $HOME/dist/*.whl`
-cd "`dirname $0`"
+export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
 
 python <<EOF
 import verify-access-configurator
