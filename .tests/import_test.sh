ca7993918fd05a8fc7e1eb932d80997c96717df5
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-02 14:23:45 +1000

    Refactoring

--- .tests/import_test.sh
+++ .tests/import_test.sh
@@ -11,13 +11,13 @@ cd "`dirname $0`"
 
 python <<EOF
 try:
-    import verify_access_configurator
-    assert verify_access_configurator.configurator != None
-    assert verify_access_configurator.appliance != None
-    assert verify_access_configurator.container != None
-    assert verify_access_configurator.webseal != None
-    assert verify_access_configurator.access_control != None
-    assert verify_access_configurator.federation != None
+    import verify-access-configurator
+    assert verify-access-configurator.configurator != None
+    assert verify-access-configurator.appliance != None
+    assert verify-access-configurator.container != None
+    assert verify-access-configurator.webseal != None
+    assert verify-access-configurator.access_control != None
+    assert verify-access-configurator.federation != None
 except:
     assert False, "Import test failed"
 EOF
