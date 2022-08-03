4338116821205cb190582ccc1a8c65f75905d3f9
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-03 12:41:08 +1000

    Syntax errors

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-export HOME=$( realpath . )
+pip install -r dev-requirements.txt
 python setup.py sdist bdist_wheel
 #twine upload --verbose --repository-url https://eu.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-components-pypi-local/ -u $ART_API_USER -p $ART_API_KEY dist/*
 twine upload --verbose --repository-url https://na.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-isam-devops-team-pypi-local/ -u $ART_API_USER -p $ART_API_KEY dist/*
