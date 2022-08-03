aea49b5fdeb0790db7eb3f3909355b9cbc94f6cc
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-03 10:48:08 +1000

    Syntax errors

--- .tests/import_test.sh
+++ .tests/import_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PIP_EXTRA_INDEX_URL="https://${ART_API_USER}:${ART_API_KEY}@na.artifactory.swg-devops.com/artifactory/api/pypi/sec-iam-isam-devops-team-pypi-local/simple"
+pip install -r dev-requirements.txt
 python setup.py sdist bdist_wheel
 export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
 
