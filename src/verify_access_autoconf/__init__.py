491d6a01385008d104bb776726b6642d760b8bfc
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-03 12:35:12 +1000

    Syntax errors

--- src/verify_access_autoconf/__init__.py
+++ src/verify_access_autoconf/__init__.py
@@ -11,28 +11,3 @@ from .webseal import WEB_Configurator
 To start the automated configuration. use the  :func:`~Configurator.configure` method.
 """
 configurator = Configurator()
-
-"""
-:var app: Can be used to configure appliance deployments system properties.
-"""
-app = Appliance_Configurator()
-
-"""
-:var docker: Can be used to configure container deployment system properties.
-"""
-cont = Docker_Configurator()
-
-"""
-:var aac: Can be used to configure the Advanced Access Control module.
-"""
-aac = AAC_Configurator()
-
-"""
-:var fed: Can be used to configure Federation module.
-"""
-fed = FED_Configurator()
-
-"""
-:var web: can be used to configure the WebSEAL Reverse Proxy module.
-"""
-web = WEB_Configurator()
