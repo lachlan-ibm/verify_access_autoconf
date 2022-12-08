7f9a59f6e41bfc77b5ea9f81d5627bd45822e6fa
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-12-08 14:04:22 +1000

    debuggign

--- src/verify_access_autoconf/configure.py
+++ src/verify_access_autoconf/configure.py
@@ -33,7 +33,7 @@ class ISVA_Configurator(object):
         for _ in range(10):
             try:
                 _logger.error("Trying LMI {}".format(mgmt_base_url(config_file)))
-                rsp = requests.get(mgmt_base_url(config_file), headers=HEADERS, verify=False)
+                rsp = requests.get(mgmt_base_url(config_file), verify=False)
                 _logger.error("{} {}".format(rsp.status_code, rsp.headers))
                 if rsp.status_code == 302 and 'Location' in rsp.headers and '/core/login' in rsp.headers['Location']:
                     return True
